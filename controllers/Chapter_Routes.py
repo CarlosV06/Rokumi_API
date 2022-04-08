2deb2a99f10471e5ad3b3b78c32cce0339fcc982
CarlosV06 <Carlosdubuc06@gmail.com>
2022-04-08 18:28:31 -0400

    Modification of various db models, and validations for creating a new chapter for a specific serie

--- controllers/Chapter_Routes.py
+++ controllers/Chapter_Routes.py
@@ -4,7 +4,6 @@ import datetime
 import time
 from flask import request, jsonify, Blueprint
 from models.Chapter_Model import ChapterModel
-from models.Tracking_Model import TrackingModel
 from models.User_Model import UserModel
 from models.Serie_Model import SerieModel
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
@@ -25,14 +24,17 @@ def uploadChapter():
         return jsonify(message = "Data was not given.", status = "400"), 400
     
     # LOCATION OF THE SERIE #
-    serie = SerieModel.objects(id = data['idSerie']).first()
+    serie = SerieModel.objects(name = data['serieName']).first()
     
     if not serie:
         return jsonify(message = "Serie not found.", status = "400"), 400
     
-    
     name = data['chapterName']
     chapter_number = data['chapterNumber']
+    
+    coincidence = ChapterModel.objects(name = name).first()
+    if coincidence and coincidence.chapter_number is chapter_number:
+        return jsonify(message = "Chapter already exists", status = "400"), 400
     
     newChapter = ChapterModel(name = name, chapter_number = chapter_number, serie = str(serie.id))
     newChapter.save()
