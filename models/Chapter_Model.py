3b3622b130d9151ebd249c06206b2737f7d853b2
CarlosV06 <Carlosdubuc06@gmail.com>
2022-04-08 18:28:31 -0400

    Modification of various db models, and validations for creating a new chapter for a specific serie

--- models/Chapter_Model.py
+++ models/Chapter_Model.py
@@ -10,6 +10,6 @@ class ChapterModel(DB.Document):
     # ATTRIBUTES #
     name = DB.StringField(required = True)
     released = DB.DateTimeField(default = datetime.datetime.now())
-    chapter_number = DB.StringField(default = "0")
+    chapter_number = DB.StringField(default = "0", unique = True)
     pages = DB.ListField(DB.StringField(), default = [])
     serie = DB.ReferenceField('SerieModel', required = True, reverse_delete_rule = mongoengine.CASCADE)
